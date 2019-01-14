2e5036ae79a88b5e89a3234f220c1cb7319074fc
weizhongyi <1226393396@qq.com>
2019-01-14 14:08:35 +0800

    feat: 增加eslint

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: ['babel-loader', 'eslint-loader']
         }
       },
       {
